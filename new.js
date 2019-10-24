b6795669a45b054700928bfe2664b738e2dc49ca
Brennan Walsh <56426721+bjw3a@users.noreply.github.com>
2019-10-23 23:41:15 -0500

    Add files via upload

---
+++ new.js
@@ -0,0 +1,21 @@
+function runCode(){
+    
+    var name = prompt('are you cool'); if(name =='yes') {alert('then you can come to my birthday party'); } else {
+        alert('then you cannot come to my party')
+    }
+        
+    
+    
+}
+function goDaddy(){
+    var x = prompt("how old are you?"); if(x>16) {alert('you can drive');} if(x < 16) {alert("you can't drive");}
+}
+function letsDoit(){
+    var name=prompt('what is your name'); if (name=='brennan') {alert('you are cool'); } else { alert('you are not cool'); }
+}
+function newCode(){
+    var y= prompt('guess what number I am thinking of'); if (y==10) {
+        alert('you guessed correctly'); 
+    } else { alert('wrong'); }
+}
+
---
+++ newscript.js
@@ -0,0 +1,21 @@
+function runCode(){
+    alert("hey hello there");
+}
+ function runDick(){
+     
+    var name = prompt('are you cool'); if(name=='yes') {alert('then you can come to my party'); } else {
+        alert('Then you cannot come to my party')
+    }
+    
+ } 
+ 
+ 
+ 
+function goDaddy(){
+    var x = prompt('how old are you?'); if(x > 40) { alert('you are an old bastard');}
+    else { alert('you are a young gun baby');}
+}
+
+function runCode2(){
+    var pill=prompt('where do you live?'); {alert('well, the ghouls are coming for you, happy halloween!!!');}
+}
